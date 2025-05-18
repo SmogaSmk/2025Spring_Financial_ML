bfe95191ecd0477b168f4304b78f6a904c62c85b
SmogaSmk <smk631124960@gmail.com>
2025-05-18 15:15:21 +0800

    Data Update

---
+++ utils/smkWork/factor.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+# 应计利润
+## 资产负债表 
+### 筛选条件[统计截止日期] 
+### 报表类型为A
+def balance_sheet(file_path):
+    pass 
+
+## 现金流分析
+### 筛选条件[统计截止日期] 
+### 报表类型为A
+def cash_flow(file_path):
+    pass 
+
+## 利润表
+### 筛选条件[统计截止日期] 
+### 报表类型为A
+def profit_statement(file_path):
+    pass 
+
+
+
+
+
+
